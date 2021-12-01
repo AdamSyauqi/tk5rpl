229a5eaefe7b1f3505710caf3f566a73035b03c5
Adam Syauqi <adam.syauqi@ui.ac.id>
2021-12-01 21:03:07 +0700

    Added navbars and home skeleton page

--- freelanceAdPage/urls.py
+++ freelanceAdPage/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "freelanceAdPage"
+
 urlpatterns = [
     path('browse_page', views.browse_page, name='browse_page'),
 ]
--- freelanceadvertisement/urls.py
+++ freelanceadvertisement/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('adpage/', include('freelanceAdPage.urls')),
+    path('', include('home.urls'))
 ]
