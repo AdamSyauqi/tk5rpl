88c429d73f2144961f98dd715c2643495b434cbb
Bimasena Putra <bimasenaputra@gmail.com>
2021-12-08 10:49:08 +0700

    Add Chat Read and Update Feature

--- chat/urls.py
+++ chat/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import chat
+from .views import chat, room, send, getmessages
 
 urlpatterns = [
     path('', chat, name='chat'),
+    path("room/<str:friend>",room,name='room'),
+    path("send",send,name='send'),
+    path("getmessages/<str:friend>",getmessages,name='getmessages'),
 ]
